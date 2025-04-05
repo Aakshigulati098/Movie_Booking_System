57be4146a07475d7a85159592cee20705926f701
Abir Saha <abirsaha453@gmail.com>
2025-04-05 21:29:58 +0530

    hey some changes have been added

---
+++ src/main/java/com/example/movie_booking_system/dto/UserDTO.java
@@ -0,0 +1,35 @@
+package com.example.movie_booking_system.dto;
+
+public class UserDTO {
+    private String name;
+
+    private String phone;
+
+    // Constructors
+    public UserDTO() {}
+
+    public UserDTO(String name, String email, String phoneNumber) {
+        this.name = name;
+
+        this.phone = phoneNumber;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
